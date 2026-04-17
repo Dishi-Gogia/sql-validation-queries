d64875a2dec3258086c9b7feb76f38fbe5d34ebf
Dishi Gogia <119417287+Dishi-Gogia@users.noreply.github.com>
2026-04-18 00:59:56 +0530

    Create basic_select_queries.sql

---
+++ basic_select_queries.sql
@@ -0,0 +1,10 @@
+-- Select all columns
+SELECT * FROM Employees;
+
+-- Select specific columns
+SELECT employee_id, employee_name, salary
+FROM Employees;
+
+-- Distinct values
+SELECT DISTINCT department
+FROM Employees;
