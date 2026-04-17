bd7246a7693391037bfa7cbf5011d122b4ad0aea
Dishi Gogia <119417287+Dishi-Gogia@users.noreply.github.com>
2026-04-18 02:32:06 +0530

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -9,3 +9,9 @@ SELECT e.employee_name, d.department_name
 FROM Employees e
 LEFT JOIN Departments d
 ON e.department_id = d.department_id;
+
+-- Right Join
+SELECT e.employee_name, d.department_name
+FROM Employees e
+RIGHT JOIN Departments d
+ON e.department_id = d.department_id;
